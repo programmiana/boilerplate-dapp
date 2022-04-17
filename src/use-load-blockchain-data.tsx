c8772adee4ba48d9f7631fc5912c0aa0b989f6d1
Mariana Moreira <m.camargomoreira@gmail.com>
2022-04-18 00:39:34 +0200

    add check in hook

--- src/use-load-blockchain-data.tsx
+++ src/use-load-blockchain-data.tsx
@@ -1,27 +1,23 @@
 import { useEffect, useState } from "react";
 import Web3 from "web3";
-declare var window: any;
-// needed for ts
 
 export function useLoadBlockchainData({ eth }) {
   const [network, setNetwork] = useState<string>("");
   const [accounts, setAccounts] = useState<string[]>([]);
   const web3 = new Web3(Web3.givenProvider || "http://127.0.0.1:7545");
 
+  if (!eth) return false;
+
   useEffect(() => {
-    if (eth) {
-      web3.eth.net
-        .getNetworkType()
-        .then((networkType) => setNetwork(networkType));
-    }
+    web3.eth.net
+      .getNetworkType()
+      .then((networkType) => setNetwork(networkType));
   }, []);
 
   useEffect(() => {
-    if (eth) {
-      web3.eth
-        .getAccounts()
-        .then((accountNumbers) => setAccounts(accountNumbers));
-    }
+    web3.eth
+      .getAccounts()
+      .then((accountNumbers) => setAccounts(accountNumbers));
   }, []);
 
   return { web3, network, accounts };
